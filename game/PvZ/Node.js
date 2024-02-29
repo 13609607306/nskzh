ee8b487213e12fe7f5115fa141f4b832cadd5165
nskzh <122281560+nskzh@users.noreply.github.com>
2024-02-29 20:24:01 +0800

    Update Node.js

--- game/PvZ/Node.js
+++ game/PvZ/Node.js
@@ -1,19 +1,19 @@
-// server.js
 const express = require('express');
 const { exec } = require('child_process');
-const app = express();
-const port = 3000;
 
-app.get('/execute', (req, res) => {
-  exec('game\\PvZ\\PvZ-BT.exe', (error, stdout, stderr) => {
+const app = express();
+const PORT = 3000;
+
+app.get('/execute-exe', (req, res) => {
+  exec('game/PVZ/PVZ-BT.exe', (error, stdout, stderr) => {
     if (error) {
-      return res.status(500).send(`Error executing the .exe file: {error}`);
+      res.send('Error executing exe file');
+      return;
     }
-    
-    res.send(`Execution result: {stdout}`);
+    res.send('Exe file executed successfully');
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
