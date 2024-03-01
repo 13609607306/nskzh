2f92f0e0cc604da5530abb314eeb4ab0a28b276b
nskzh <122281560+nskzh@users.noreply.github.com>
2024-03-01 22:02:06 +0800

    Add files via upload

---
+++ game/Slay/SlayTheSpireTroubleshoot.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Launching the game in troubleshoot mode.
+echo This might take a while...
+
+mkdir sendToDevs
+jre\bin\java.exe -jar desktop-1.0.jar > sendToDevs\troubleshoot.log 2>&1
+
+type sendToDevs\troubleshoot.log
+
+dxdiag /dontskip /whql:off /t %CD%\sendToDevs\dxdiag.txt
+echo "There should now be a 'troubleshoot.log' file that you can send the devs."
+pause
